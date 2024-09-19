91d08c2bb71aac1e94277d0e249c04b8b41936d7
Allison Holt <100834+allizon@users.noreply.github.com>
2024-09-18 21:56:23 -0400

    [main] Testing check for dev environment

--- src/components/ReactComponent.jsx
+++ src/components/ReactComponent.jsx
@@ -4,12 +4,33 @@
 //
 // We *could* use Typescript up in here more if need be...
 
-export default function ({ testProp, children }) {
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
+export function SwitchDemo() {
   return (
-    <div>
-      this is a <b>react</b> component {testProp}
-      <br />
-      {children}
+    <div className="flex items-center space-x-2">
+      <Switch id="airplane-mode" />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
     </div>
+  );
+}
+
+export default function ({ testProp, children }) {
+  console.log(window.location.href);
+  const isDev = window.location.href.includes("localhost");
+
+  return (
+    <>
+      <div>
+        this is a <b>react</b> component {testProp}
+        <br />
+        {isDev ? "DEV ENV" : ""}
+      </div>
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
+    </>
   );
 }
